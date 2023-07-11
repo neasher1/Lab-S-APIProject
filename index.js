b4942051ea1a152fd4afec658996ebeca1d8c66f
neasher1 <neasher75@gmail.com>
2023-07-11 07:44:01 +0600

    schema and db connection

--- index.js
+++ index.js
@@ -8,6 +8,26 @@ const port = 5000;
 const APIApp = express.Router();
 app.use("/", APIApp);
 
+mongoose
+  .connect(
+    `mongodb+srv://apiApp:PWrx4ddwp84ZalFi@cluster0.hlzaati.mongodb.net/APIApp?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Connected to Database");
+  })
+  .catch((error) => {
+    console.error("Error connecting to DB:", error);
+  });
+
+
+const productSchema = new mongoose.Schema({
+    name: String, 
+    price: Number,
+    description: String,
+});
+
+const Product = mongoose.model('Product', productSchema);
+
 APIApp.get("/", (req, res) => {
   res.send("Test Server");
 });
